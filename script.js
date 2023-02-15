6b312423e8e10ecc22ca132c112d623e0d2ab33b
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-02-15 11:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 //your code here
 var btn = document.getElementById("btn");
 var displayRandomNumber = document.getElementById("num");
-var guessedNumber = document.getElementById("guess");
+var guessedNumber = document.getElementById("guess").value;
+var respond = document.getElementById("respond");
+
+
 
 
 function generateRandomNumber(){
 	// Math.random gives a decimal value, we use the floor value to remove the 
 	var num = Math.floor(Math.random() * 41) - 20;
 	displayRandomNumber.textContent = num;
+	
 }
 btn.addEventListener("click", generateRandomNumber);
 
