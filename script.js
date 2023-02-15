6c3fb70f4a5c0cdad33cea827a190d2336552c97
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-02-15 11:06:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your code here
 var btn = document.getElementById("btn");
+var displayRandomNumber = document.getElementById("num");
 
 function generateRandomNumber(){
-	
+	var num = Math.floor((Math.random() * 41) - 20);
+	displayRandomNumber.textContent = num;
 }
 btn.addEventListener("click", generateRandomNumber);
+
