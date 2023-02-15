4e51b8a7b3b772ecd14f80b47e270200f03d1027
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-02-15 12:55:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("btn");
 var randomNum = document.getElementById("num");
-var input = document.getElementById("guess").value;
+var input = document.getElementById("guess");
 var res = document.getElementById("respond");
 
 function check(rand, input){
@@ -17,7 +17,7 @@ function check(rand, input){
 function generateRandomNumber(){
 	var rand = Math.floor(Math.random() * 41) - 20;
 	randomNum.textContent = rand;
-	check(rand, input);
+	check(rand, input.value);
 }
 
 btn.addEventListener('click', generateRandomNumber);
